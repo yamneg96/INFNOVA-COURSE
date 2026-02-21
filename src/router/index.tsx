dc0396b9191fa291cbee3738463e2466d3ad0293
Yamlak Negash <yamlaknegash96@gmail.com>
2026-02-21 23:12:48 +0300

    Noticed and updated the scroll issue while navigation

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import CoursesPage from '../pages/CoursesPage';
 import CourseDetailPage from '../pages/CourseDetailPage';
 
+import React from 'react';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 export default function AppRouter() {
   return (
-    <Routes>
-      <Route path="/" element={<CoursesPage />} />
-      <Route path="/courses/:id" element={<CourseDetailPage />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<CoursesPage />} />
+        <Route path="/courses/:id" element={<CourseDetailPage />} />
+      </Routes>
+    </>
   );
 }
